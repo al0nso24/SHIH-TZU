16752e8395bff2baf37da19c6b2353d8f272116b
Alonso Zegarra Velasquez <zegarrav24@gmail.com>
2026-03-03 16:14:48 -0500

    fix railway

--- servidor/serv.js
+++ servidor/serv.js
@@ -4,12 +4,13 @@ const mysql = require("mysql2");
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 })
 
 
