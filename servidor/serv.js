909fec5ec6e3a41fca0dbe98a2ba61793b2162a2
Alonso Zegarra Velasquez <zegarrav24@gmail.com>
2026-03-03 17:12:26 -0500

    ayudame Dios

--- servidor/serv.js
+++ servidor/serv.js
@@ -5,13 +5,15 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-})
+    port: process.env.MYSQLPORT,
+    waitForConnections: true,
+    connectionLimit: 10
+});
 
 
 //Agregar perrito a la base de datos:
